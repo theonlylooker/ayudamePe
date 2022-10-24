5677a12e574bf3b66bca8ea846ab720a84f509e7
basurco eduardo <angelsoulsword@gmail.com>
2022-10-24 12:36:37 -0500

    init

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,10 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
-
+import React from "react";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="text-3xl text-blue-600">
+      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat corrupti
+      tempora tenetur deserunt quasi autem illum? Praesentium quae officia neque
+      odit adipisci quos illo ex minus esse! Aut, delectus ducimus!
     </div>
   );
 }
