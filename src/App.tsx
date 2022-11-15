e806a6925ed96c291266fd3bbf8695da81c62e87
basurco eduardo <angelsoulsword@gmail.com>
2022-11-15 12:03:06 -0500

    homepage mobile and desktop

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,9 @@
 import React from "react";
+import Homepage from "./pages/Homepage";
 function App() {
   return (
-    <div className="text-3xl text-blue-600 text font-inter bg-first">
-      Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellat corrupti
-      tempora tenetur deserunt quasi autem illum? Praesentium quae officia neque
-      odit adipisci quos illo ex minus esse! Aut, delectus ducimus!
-      <p>nuevo</p>
-      <p>nuevo2</p>
+    <div>
+      <Homepage />
     </div>
   );
 }
